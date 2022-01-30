252a5a01b20116075874ae4e85dc62fd998b5d24
grrumblerr <grrumblerr@yandex.by>
2022-01-30 21:14:43 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,19 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  var symbols = expr.match(/.{10}/g);
+  let result = [];
+  for (let i=0; i<symbols.length; i++) {
+       if (symbols[i] == '**********') result.push(' ');
+      result.push(MORSE_TABLE[symbols[i].replace(/^0+/g, '').replace(/10/g, '.').replace(/11/g, '-')]);
+  }
+  // console.log(symbols);
+  // console.log(result);
+  return result.join('');
 }
+
+// console.log(decode("00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"))
+
 
 module.exports = {
     decode
